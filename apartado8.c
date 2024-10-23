2a082e097d4c93b4c999a23e16f0a8d239a78c49
Pepecrack1 <pablo.freijeiro@rai.usc.es>
2024-10-23 11:56:53 +0200

    2.0

--- apartado8.c
+++ apartado8.c
@@ -7,6 +7,13 @@ int main(int argc, char **argv)
     pid_t J;
     pid_t M;
     
+    FILE*fp = fopen("file.txt","w+");
+    if (file == NULL)
+    {
+        printf("Error en la apertura del archivo\n")
+        return(EXIT_FAILURE);
+    }
+    
     srand(time(NULL));  //Fijamos semilla para los numeros aleatorios
 
     J=fork();
@@ -36,36 +43,49 @@ int main(int argc, char **argv)
     }
     else if(M==0)  //-------------------------------------MAQUINA
     {   
+
+        int turno;    //Aleatorio, 0 si saca la maquina y 1 si saca el jugador
+        int posicionJ;
         int posicionM = rand() % 10; //Obtenemos posicion aleatoria entre 0 y 9
-        kill(pid_padre,SIGUSR1);
-
-        while(1) {
-            pause();
-            // instrucciones
+        
+        
+        while(puntosJ<10&&puntosM<10)
+        {
+            turno=rand() % 2;
+            puntoTerminado=false;
+            while(!puntoTerminado)
+            {
+                leerArchivo(&posicionJ,&posicionM,&posicionB);
+                turno = (turno+1)%2;
+                if(turno)
+                {
+                    if(abs(posicionM-posicionB)<=3)
+                    {
+                	    kill(J,SIGUSR2);
+                    }
+                    else
+                    {
+                        puntosJ+=1;
+                        printf("Punto para el jugador\n");
+                        printf("MARCADOR: | J %d | M %d |\n");
+                    }
+                }
+                else
+                {
+                    if(abs(posicionJ-posicionB)<=3)
+                    {
+                	    kill(K,SIGUSR1);
+                    }
+                    else
+                    {
+                        puntosM+=1;
+                        printf("Punto para la máquina\n");
+                        printf("MARCADOR: | J %d | M %d |\n");
+                    }
+                }
+            }    
         }
-
     }
-
-    int puntosM=0;
-    int puntosJ=0;
-
-
-    int turno;    //Aleatorio, 0 si saca la maquina y 1 si saca el jugador
-    while(puntosJ<10&&puntosM<10)
-    {
-        turno=rand() % 2;
-
-
-//##############################################
-        if (turno) {
-            kill(J,SIGUSR1);
-        }
-        else {
-            kill(M,SIGUSR2);
-        }
-//##############################################
-    }
-
     return 0;
 }
 
@@ -111,11 +131,40 @@ void imprimir(int posM,int posJ,int posB,int turno) {
 
 }
 
-/*
 
-"#...M......#"
-        o
-"#----------#"
-
-
-*/
+static void gestion(int numero_de_senhal)   //Manejador
+{
+    switch (numero_de_senhal)   //En funcion del numero de señal hariamos una cosa u otra
+    {
+        case SIGUSR1:
+            int posicionJ, posicionM, posicionB;
+            if(fscanf(file, "Maquina: %d | Jugador: %d | Bola %d\n", &posicionM, &posicionJ, &posicionB) == EOF) 
+            {
+                perror("ERROR leyendo del archivo\n");
+            }
+    
+            printf("Introduce la posicion a la que quieres lanzar: ");
+            scanf(" %d", &posicionB);
+            printf("Introduce la posicion a la que te deseas mover: ");
+            scanf(" %d", &posicionJ);
+            
+            printf("El jugador se mueve a la posicion %d y envía la bola hacia la posicion %d\n",posicionM,posicionB);
+            //Mostrar mesa
+            break;
+            
+        case SIGUSR2:
+            int posicionJ, posicionM, posicionB;
+            if(fscanf(file, "Maquina: %d | Jugador: %d | Bola %d\n", &posicionM, &posicionJ, &posicionB) == EOF) 
+            {                perror("ERROR leyendo del archivo\n");
+            }
+            posicionB= rand() % 10;
+            posicionM += (rand()%3) * pow(-1, rand()%2);
+            
+            printf("La maquina se mueve a la posicion %d y envía la bola hacia la posicion %d\n",posicionM,posicionB);
+            //Mostrar mesa
+            break;
+            
+        case SIGTERM:
+            exit(0);
+            break;
+      }
