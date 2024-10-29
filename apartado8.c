0ac0b0e8afadb7b3671b9ee80e5b495c737c9add
Pepecrack1 <pablo.freijeiro@rai.usc.es>
2024-10-29 18:53:48 +0100

    Comentado el 8

--- apartado8.c
+++ apartado8.c
@@ -11,34 +11,35 @@
 static void gestionHijos(int numero_de_senhal);
 static void gestionPadre(int numero_de_senhal);
 void imprimirMesa(int posM,int posJ,int posB,int turno, int puntosJ, int puntosM);
-
-FILE*file;
 pid_t padre;
 
+//Función para leer la última línea, con los datos más recientes, del archivo
 void leerArchivo(int*posicionJ, int*posicionM, int*posicionB,int*turno,int*puntosJ,int*puntosM)
 {    
     char linea[1024];
-    FILE*file = fopen("file.txt","r");
+    FILE*file = fopen("file.txt","r");  //Abrimos el archivo en modo lectura
     
-    if (file == NULL)
+    if (file == NULL) //comprobamos errores
     {
         perror("Error en la apertura del archivo\n");
         kill(padre,SIGTERM);
     }
     
-    fseek(file,0,SEEK_SET);
+    fseek(file,0,SEEK_SET); //Movemos el puntero al principio del archivo, por seguridad
     
     while (fgets(linea, 1024, file) != NULL); //Leemos líneas del archivo hasta llegar a la última, por si acaso ha pasado algo, pero normalmente solo habrá una línea de cada vez
     
-    if(sscanf(linea, "Maquina: %d | Jugador: %d | Bola: %d | Turno: %d | PuntosJ: %d | PuntosM: %d", posicionM, posicionJ, posicionB, turno, puntosJ, puntosM)!=6)
+    if(sscanf(linea, "Maquina: %d | Jugador: %d | Bola: %d | Turno: %d | PuntosJ: %d | PuntosM: %d", posicionM, posicionJ, posicionB, turno, puntosJ, puntosM)!=6)  //Leemos la última línea y comprobamos errores
     {
         perror("Error leyendo la línea cogida del archivo (sscanf)\n");
-        kill(padre,SIGTERM);
+        kill(padre,SIGTERM);  //Terminamos al padre su hay error
     }
     
-    fclose(file);
+    fclose(file); //Cerramos el archivo
 }
 
+
+//Función para imprimir una línea, con los datos de un instante, en el archivo
 void imprimirArchivo(int posicionJ,int posicionM,int posicionB,int turno,int puntosJ,int puntosM)
 {
     char linea[1024];
@@ -50,15 +51,15 @@ void imprimirArchivo(int posicionJ,int posicionM,int posicionB,int turno,int pun
         kill(padre,SIGTERM);
     }
     
-    fseek(file,0,SEEK_END); //Movemos puntero al final del archivo
+    fseek(file,0,SEEK_END); //Movemos puntero al final del archivo, por seguridad
     
     snprintf(linea, sizeof(linea),"Maquina: %d | Jugador: %d | Bola: %d | Turno: %d | PuntosJ: %d | PuntosM: %d\n", posicionM, posicionJ, posicionB, turno, puntosJ, puntosM);
+    //Formateamos la línea que se va a imprimir en el archivo
     
-    fputs(linea,file);
-    fclose(file);
+    fputs(linea,file);  //La escribimos
+    fflush(file);   //Nos aseguramos de que es escribe todo en el archivo
+    fclose(file);   //Cerramos la conexión con el archivo
 }
-
-pid_t padre;
 
 
 int main(int argc, char **argv) 
@@ -70,7 +71,7 @@ int main(int argc, char **argv)
     
     srand(time(NULL));  //Fijamos semilla para los numeros aleatorios
 
-    J=fork();
+    J=fork(); //Primer fork, genera el hijo jugador
     
     if(J<0)
     {
@@ -79,13 +80,13 @@ int main(int argc, char **argv)
     }
     else if(J==0)  //-------------------------------------JUGADOR
     {   
-        if (signal(SIGUSR1, gestionHijos) == SIG_ERR) printf("Error al vincular hijo1\n");
+        if (signal(SIGUSR1, gestionHijos) == SIG_ERR) printf("Error al vincular hijo1\n");  //Asignamos manejador a SIGUSR1 en el jugador
         
-        while(1);
+        while(1); //Entra en bucle infinito, interrumpido ocacionalmente por las señales que lleguen
         exit(0);
     }
 
-    M=fork();
+    M=fork(); //Fork para generar el segundo hijo, la máquina
 
     if(M<0)
     {
@@ -94,15 +95,15 @@ int main(int argc, char **argv)
     }
     else if(M==0)  //-------------------------------------MAQUINA
     {   
-        if (signal(SIGUSR2, gestionHijos) == SIG_ERR) printf("Error al vincular hijo2\n");
+        if (signal(SIGUSR2, gestionHijos) == SIG_ERR) printf("Error al vincular hijo2\n");  //Le asignamos manejador a SIGUSR2 (usamos el mismo que antes, pero con una señal distinta)
         
         while(1);
         exit(0);
     }
     
-    if(signal(SIGUSR1, gestionPadre) == SIG_ERR) printf("Error al vincular padre\n");
+    if(signal(SIGUSR1, gestionPadre) == SIG_ERR) printf("Error al vincular padre\n"); //Usaremos SIGUSR1 también en el padre, pero en este caso la señal no hará nada, simplemente se envía por parte de los hijos para indicar al padre que debe actuar, terminando su espera
     
-    int turno;    //Aleatorio, 0 si saca la maquina y 1 si saca el jugador
+    int turno;    //Aleatorio al principio, 0 si golpea la maquina y 1 si es el jugador
     int puntosJ=0;
     int puntosM=0; 
     int posicionJ;
@@ -110,21 +111,21 @@ int main(int argc, char **argv)
     int posicionM;
     
      
-    while( (puntosJ<11&&puntosM<11) || abs(puntosJ-puntosM) < 2 )
+    while(puntosJ<2&&puntosM<2) //El partido es a 10 puntos, cuando alguno llegue a 10, se sale del bucle
     {
-        int puntoTerminado=0;
+        int puntoTerminado=0; //Se usará esta variable para indicar si se ha terminado un punto o no
         turno=rand() % 2;   //Escogemos de forma aleatoria quien tiene el turno
         
-        posicionM=rand() % 10; //Obtenemos posicion aleatoria entre 0 y 9 para la máquina
-        
+        posicionM=rand() % 10; //Obtenemos posicion aleatoria entre 0 y 9 para la máquina, su posición inicial
         printf("La máquina empieza en la posición %d\n",posicionM);
-        do
+        
+        do  //Pedimos al jugador su posición inicial, y la restringimos al valor adecuado
         {   
             printf("Escoge tu posición de inicio (0-9): ");
             scanf(" %d",&posicionJ);
         }while(posicionJ<0||posicionJ>9);
         
-        if(turno) //Si saca el jugador
+        if(turno) //Si saca el jugador: turno==1
         {
             posicionB=posicionJ;  //Colocamos la bola justo delante para que saque
             printf("Comienza el punto, saca el jugador:\n");
@@ -135,24 +136,23 @@ int main(int argc, char **argv)
             printf("Comienza el punto, saca la máquina:\n") ;
         }
         
-        imprimirMesa(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);
+        imprimirMesa(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);  //Imprimimos estado de la mesa al inicio del punto
         
         imprimirArchivo(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);  //Imprimimos los datos iniciales en el archivo
         
         
         
-        while(!puntoTerminado)
+        while(!puntoTerminado)  //Mientras no acaba el punto
         {   
-            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM);
-            if(turno)
+            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM); //Leemos información del archivo
+            if(turno) //Si es el turno del jugador
             {
-                if(abs(posicionJ-posicionB)<=3)
+                if(abs(posicionJ-posicionB)<=3) //Si el jugador puede llegar a la bola, se la manda SIGUSR1 para que actúe
                 {
             	    kill(J,SIGUSR1);
             	    pause();
-            	    //sleep(1);
                 }
-                else
+                else  //Si no llega, se le suma un punto a la máquina, se termina el punto, y se muestra el marcador
                 {
                     puntosM+=1;
                     puntoTerminado=1;
@@ -162,15 +162,14 @@ int main(int argc, char **argv)
                     printf("MARCADOR: | J %d | M %d |\n\n",puntosJ,puntosM);
                 }
             }
-            else
+            else  //Si le toca a la máquina
             {
-                if(abs(posicionM-posicionB)<=3)
+                if(abs(posicionM-posicionB)<=3)   //si llega a la bola, se manda SIGUSR2 para que actúe
                 {
             	    kill(M,SIGUSR2);
             	    pause();
-            	    //sleep(1);
                 }
-                else
+                else    //Si no, pierde el punto, y se acaba
                 {
                     puntosJ+=1;
                     puntoTerminado=1;
@@ -180,19 +179,25 @@ int main(int argc, char **argv)
                     printf("MARCADOR: | J %d | M %d |\n\n",puntosJ,puntosM);
                 }
             }
+        //Ahora, si el punto no ha terminado, se volvería a entrar en el bucle, para seguir con los demás golpes
         }
+    //Si ninguno de los 2 ha llegado a 10, se inicia un nuevo punto (se da otra vuelta al bucle)
     }
+    
+    //En cuanto uno llegue a 10, se termina la partida
     printf("\nPARTIDA TERMINADA\n");
-    if(puntosM < puntosJ)
+    
+    if(puntosM < puntosJ) //Imprimimos el resultado en función de quién gana
     {
         printf("Gana el jugador: %d - %d , bien jugado!\n",puntosJ, puntosM);
     }
-    else printf("Gana la máquina: %d - %d\n , mala suerte!\n",puntosJ, puntosM);
+    else printf("Gana la máquina: %d - %d , mala suerte!\n",puntosJ, puntosM);
     
     return (EXIT_SUCCESS);
 }
 
 
+//Función para imprimir el estado de la mesa en un momento dado
 void imprimirMesa(int posJ,int posM,int posB,int turno, int puntosJ, int puntosM) {
 
     printf("     J %d - %d M \n\n",puntosJ,puntosM);  //Imprimimos marcador
@@ -236,20 +241,16 @@ void imprimirMesa(int posJ,int posM,int posB,int turno, int puntosJ, int puntosM
 
 }
 
-static void gestionPadre(int numero_de_senhal)
+//Función de gestión para el padre, asociada a SIGUSR1, no hace nada, simplemente es para romper la espera
+static void gestionPadre(int numero_de_senhal)  
 {
-    /*switch(numero_de_senhal)
-    {
-        case SIGUSR1:
-            printf("SEÑAL PADRE\n");
-            break;
-    }*/
 }
 
+//FUnción para mover al jugador
 void moverJugador(int*posicionJ)
 {
     int posicionMover;
-    do
+    do  //Pedimos cómo se quiere mover
     {   
         printf("Introduce cómo te quieres mover (desde -2 a +2): ");
         scanf(" %d", &posicionMover);
@@ -260,64 +261,65 @@ void moverJugador(int*posicionJ)
     if(*posicionJ>9) *posicionJ=9;
 }
 
+//Función para mover a la máquina
 void moverMaquina(int*posicionM)
 {
     int posicionMover = rand()%3; //Se mueve numero aleatorio entre 0 y 2
-    if(rand()%2) posicionMover *= (-1); //Multiplicamos por -1 o no, aleatorio
+    if(rand()%2) posicionMover *= (-1); //Multiplicamos por -1 o no, aleatorio, para ver hacia qué lado va
     *posicionM += posicionMover;
     if(*posicionM<0) *posicionM=0;  //Si se sleep(1);pasa de los límites, colfflush(file);ocamos al jugador en el extremo correspondiente
     if(*posicionM>9) *posicionM=9;
 }
 
-static void gestionHijos(int numero_de_senhal)  //Manejador, NO PUEDE HACER RETURN-----------------------------
+//Función manejadora para los hijos
+static void gestionHijos(int numero_de_senhal)
 {
     int posicionJ, posicionM, posicionB, turno, puntosJ, puntosM;
     
     switch (numero_de_senhal)   //En funcion del numero de señal hariamos una cosa u otra
     {
-        case SIGUSR1:
-            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM);
+        case SIGUSR1: //Para el hijo 1 (jugador)
+            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM); //Leemos última línea del archivo
             
-            printf("La bola viene hacia ti, te toca golpear:\n");
-            do
+            printf("Jugador, te toca golpear:\n");
+            do    //Se le pide posición a la que va a lanzar la bola
             {   
                 printf("Introduce la posicion a la que quieres lanzar: ");
                 scanf(" %d", &posicionB);
                 printf(" %d\n", posicionB);
             }while(posicionB<0||posicionB>9);
             
-            moverJugador(&posicionJ);
+            moverJugador(&posicionJ);   //Movemos jugador y máquina
             moverMaquina(&posicionM);
             
-            turno = (turno+1)%2;
-            imprimirArchivo(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);
+            turno = (turno+1)%2;  //Obtenemos el siguiente turno
+            imprimirArchivo(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);   //Imprimimos los datos en el archivo, y mostramos por pantalla el estado de la partida
             printf("El jugador se mueve a la posicion %d y envía la bola hacia la posicion %d\n",posicionJ,posicionB);
             imprimirMesa(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);
             
             
-            kill(padre,SIGUSR1);
+            kill(padre,SIGUSR1);  //Indicamos al padre que vuelva a trabajar
             break;
             
-        case SIGUSR2:
-            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM);
+        case SIGUSR2:   //Para el hijo 2 (maquina)
+        
+            leerArchivo(&posicionJ,&posicionM,&posicionB,&turno,&puntosJ,&puntosM);   //leemos información del archivo
             
-            posicionB= rand() % 10; //Manda bola a posición aleatoria
+            posicionB= rand() % 10; //Manda bola a posición aleatoria de 0 a 9
             
             printf("Le toca golpear a la máquina, prepárate:\n");
-            moverJugador(&posicionJ);
+            
+            moverJugador(&posicionJ);   //Movemos al jugador y a la máquina
             moverMaquina(&posicionM);
             
-            turno = (turno+1)%2;
-            imprimirArchivo(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);
-            printf("La maquina se mueve a la posicion %d y envía la bola hacia la posicion %d\n",posicionM,posicionB);
+            turno = (turno+1)%2;    //Pasamos turno
+            imprimirArchivo(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);   //imprimimos información en el archivo
+            printf("La maquina se mueve a la posicion %d y envía la bola hacia la posicion %d\n",posicionM,posicionB);    //Sacamos por pantalla el estado de la partida
             imprimirMesa(posicionJ,posicionM,posicionB,turno,puntosJ,puntosM);
             
             
-            kill(padre,SIGUSR1);
+            kill(padre,SIGUSR1);    //Enviamos SIGUSR1 al padre, para que continúe
             break;
             
-        case SIGTERM:
-            exit(0);
-            break;
       }
 }
